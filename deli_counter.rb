22be77ed1a335073977a57fa18a472944f887fe4
Joelle Helm <joellenhelm@gmail.com>
2019-06-20 17:38:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,9 +8,9 @@ def line(katz_deli)
   end
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else
-    puts "The line is currently: " + linestr.strip
-end
+    else
+      puts "The line is currently: " + linestr.strip
+  end
 end
 
 
@@ -23,8 +23,8 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
-  else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
-end
+    else
+      puts "Currently serving #{katz_deli[0]}."
+      katz_deli.shift
+  end
 end
