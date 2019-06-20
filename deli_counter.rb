066ee22f5b0cdb1f31f0d67c2da281eaf4ba4535
Joelle Helm <joellenhelm@gmail.com>
2019-06-20 17:26:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + linestr
+    puts "The line is currently: " + linestr.strip
 end
 end
 
