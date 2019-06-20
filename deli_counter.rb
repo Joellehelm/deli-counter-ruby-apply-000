14886428f8951aa4014363db7153ea3cd4c30d0c
Joelle Helm <joellenhelm@gmail.com>
2019-06-20 17:16:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,31 +1,18 @@
 katz_deli = []
-
+linestr = ""
 def line(katz_deli)
-  linestr = ""
-  katz_deli.each_with_index do |person, idx|
-    linestr += "#{idx.to_s}. #{person}"
+  katz_deli.each do |name|
+    spot = name.index + 1
+    linestr += "#{spot}. #{name}"
   end
-  if katz_deli.length != 0
-    puts "The line is currently: " + linestr
-  elsif katz_deli.length == 0
+  if katz_deli.length == 0
     puts "The line is currently empty."
-end
-
+  else 
+    puts "The line is currently: " + linestr
+end 
 end
 
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
-  place = katz_deli[name]
-
-  puts "Welcome, #{name}. You are number #{place} in line."
-  place += 1
-
-end
-
-def now_serving(katz_deli)
-
-  "Currently serving #{katz_deli[0]}."
-
-
-end
+  
+end 
