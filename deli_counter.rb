046afb0a7ed301cb6bced4a47cba12fd44f50918
Joelle Helm <joellenhelm@gmail.com>
2019-06-20 17:35:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,6 @@ katz_deli = []
 
 def line(katz_deli)
   linestr = ""
-
   katz_deli.each do |name|
     spot = katz_deli.index(name) + 1
     linestr += "#{spot}. #{name} "
@@ -18,4 +17,13 @@ end
 def take_a_number(katz_deli, name)
  katz_deli << name
  puts "Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line."
+end
+
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
 end
